f54fca29229934ffd5da48029ab136443190ccae
El Thomas <thomas.folmer@outlook.fr>
2017-10-13 17:31:31 +0200

    bugfix & comments

--- app/src/main/java/fr/dta/formtion/calculettepolonaise/MainActivity.java
+++ app/src/main/java/fr/dta/formtion/calculettepolonaise/MainActivity.java
@@ -1,6 +1,5 @@
 package fr.dta.formtion.calculettepolonaise;
 
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +13,17 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+    /*
+        This is a Polish Calculator. Regarding how it works, the user can put a number between 0 and
+        around 999 999 999 (limited by the size of Integer, any larger number would cause the app to
+        crash).
+        The user writes a number in the inputView.
+        All operations are kept in the stackCalculator
+        The listView is in charge of displaying the number in the stackCalculator
+
+        Created by Thomas FOLMER with almost no coffee...  :'(
+     */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Assigantion of the numerous buttons
         button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
@@ -73,6 +84,11 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listV);
 
 
+        /*
+            Declaration of one OnClick in charge of the buttons, on for the operators (+,-,*,/) and
+            a last one targeted at the buttons that are going to manipulate the stack
+         */
+
         View.OnClickListener numberListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,6 +113,9 @@ public class MainActivity extends AppCompatActivity {
         };
 
 
+        /*
+            Assignation of the onClickListener
+         */
         button0.setOnClickListener(numberListener);
         button1.setOnClickListener(numberListener);
         button2.setOnClickListener(numberListener);
@@ -125,6 +144,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /*
+        Method in charge of filling the input field (where the number is created and before it is
+        sent to the stack
+     */
+
     private void displayNumber(View v){
         String number = v.getTag().toString();
         String prev = this.inputView.getText().toString();
@@ -132,17 +156,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //update the stack view with the new input  and clear the input view
-    private void displayStack(){
-        String num = this.inputView.getText().toString();
-        Log.d("display", "Le nombre à ajouter à la display stack est :"+num);
-        updateListView();
-        emptyInputView();
+    /*
+        Method in charge of adding the number to the stack
+     */
 
-
-
-    }
-    // Display the number hit by the user in the input view
     private void addNumber() {
 
         String num = this.inputView.getText().toString();
@@ -150,34 +167,49 @@ public class MainActivity extends AppCompatActivity {
         this.stackCalculette.add(number);
     }
 
-    //Method implementing the operande operations
+
+  /*
+        Trigerred by the onClickListener, this method is going to perform the operation asked
+        and update the stack
+   */
     private void operation(View v) {
-        String input = v.getTag().toString();
-        if(this.stackCalculette.size()>=2){
+        String input = v.getTag().toString(); // the button send its function through a tag
+        if(this.stackCalculette.size()>=2){   // check if a calculation can be made
+
             if(input.equals("+")){
+                /*
+                   -  Gather the last number in order to add it to the one before.
+                   -  The result is set as the last number and the other is removed
+                   -  The stack is updated
+                 */
                 int result = this.stackCalculette.get(this.stackCalculette.size()-1) + this.stackCalculette.get(this.stackCalculette.size()-2);
                 this.stackCalculette.remove(this.stackCalculette.size()-2);
                 this.stackCalculette.set(this.stackCalculette.size()-1,result);
-
                 updateListView();
             }
             if(input.equals("-")){
+                /*
+                      Same as above
+                 */
                 int result = this.stackCalculette.get(this.stackCalculette.size()-1) - this.stackCalculette.get(this.stackCalculette.size()-2);
                 this.stackCalculette.remove(this.stackCalculette.size()-2);
                 this.stackCalculette.set(this.stackCalculette.size()-1,result);
-
                 updateListView();
             }
             if(input.equals("*")){
+                 /*
+                      Same as above
+                 */
                 int result = this.stackCalculette.get(this.stackCalculette.size()-1) * this.stackCalculette.get(this.stackCalculette.size()-2);
                 this.stackCalculette.remove(this.stackCalculette.size()-2);
                 this.stackCalculette.set(this.stackCalculette.size()-1,result);
-
                 updateListView();
             }
             if(input.equals("/")){
+                 /*
+                      Same as above with a check to see if the division does not include a 0
+                 */
                 if( this.stackCalculette.get(stackCalculette.size()-2)!=0  &&  this.stackCalculette.get(stackCalculette.size()-1)!=0  ){
-                    Log.d("Valeurs","Les valeurs à calculer sont : "+this.stackCalculette.get(stackCalculette.size()-2) + " et "+this.stackCalculette.get(stackCalculette.size()-1));
                     int result = this.stackCalculette.get(this.stackCalculette.size()-1) / this.stackCalculette.get(this.stackCalculette.size()-2);
                     this.stackCalculette.remove(this.stackCalculette.size()-2);
                     this.stackCalculette.set(this.stackCalculette.size()-1,result);
@@ -190,33 +222,46 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    /*
+        Clear the number being written by the user
+     */
     private void emptyInputView(){
         this.inputView.setText("");
     }
 
-
+    /*
+        Method called by the onClickListener regarding the buttons that modify the stack
+     */
     private void stackTransformation(View v) {
         String input = v.getTag().toString();
 
         if(input.equals("enter")){
+            /*
+                Method in charge of adding the number to the stack
+             */
             String number = this.inputView.getText().toString();
-
-            if(!number.equals("")){
-                if(number.length()>9){
-                    Toast.makeText(this, "Come on men, be serious! You ain't Cresus", Toast.LENGTH_SHORT).show();
+            if(!number.equals("")){             //Check if the inputView is not empty
+                if(number.length()>9){          //Check if the number is not too big (constrain of the Integers)
+                    Toast.makeText(this, "Come on men, be serious! Merci de mettre un nombre plus petit", Toast.LENGTH_SHORT).show();
+                }else{
+                    addNumber();                //Methode that add the number to the stack
+                    String num = this.inputView.getText().toString();
+                    Log.d("display", "Le nombre à ajouter à la display stack est :"+num);
+                    updateListView();           //Update the display
+                    emptyInputView();           //Clear the inputView
                 }
-                addNumber();
-                displayStack();
             }
-
         }
         if(input.equals("pop")){
+            // Remove the last number in the stack
             if(stackCalculette.size()!=0){
                 stackCalculette.remove(stackCalculette.size()-1);
                 updateListView();
             }
         }
         if(input.equals("swap")){
+            //Change the last number in the stack with the one right before
             if(stackCalculette.size()>1){
                 int previous = stackCalculette.get(stackCalculette.size()-2);
                 Log.d("stack", "l'avant dernier nombre de la stack est : "+previous );
@@ -227,23 +272,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if(input.equals("clear")){
+            //Clear the inputView
             this.inputView.setText("");
         }
 
         if(input.equals("random")){
+            //Generate a random number
             Random random = new Random();
-            int nombreAleatoire = random.nextInt(9999 - 0 );
+            int nombreAleatoire = random.nextInt(99999 - 0 );
             this.inputView.setText(String.valueOf(nombreAleatoire));
         }
 
     }
 
+    /*
+        Method in charge of updating the List View with the stack
+     */
     private void updateListView(){
         final ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(MainActivity.this,
                 android.R.layout.simple_list_item_1, stackCalculette);
         listView.setAdapter(adapter);
     }
 
+    /*
+        Methods used to save the values if the screen changes
+     */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
